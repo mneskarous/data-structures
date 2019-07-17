64b09c87e66af5bf7a57898bf7474f172166e01f
Michael Eskarous <mneskarous@gmail.com>
2019-07-17 15:10:31 -0700

    Implemented function for functional instantiation of the queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,27 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.dequeue = function() {
+    var dequeued = storage[0];
+    delete storage[0];
+    var position = Object.values(storage);
+    for (var i = 0; i < position.length - 1; i++) {
+        position[i] = position[i + 1];
+    }
+    return dequeued;
   };
 
   someInstance.size = function() {
+    var size = 0;
+      for (var key in storage) {
+        if (storage.hasOwnProperty(key)) {
+            size++;
+        }
+      }
+      return size;
   };
 
   return someInstance;
