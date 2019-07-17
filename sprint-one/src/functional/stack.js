bd075efbe7b49665b3c6125ea3fb9dbfd4c3cb0d
Michael Eskarous <mneskarous@gmail.com>
2019-07-17 15:09:57 -0700

    Implemented function for functional instantiation of the stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,23 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.pop = function() {
+    var popped = storage[someInstance.size() - 1];
+    delete storage[someInstance.size() - 1];
+    return popped;
   };
 
   someInstance.size = function() {
+    var size = 0;
+      for (var key in storage) {
+        if (storage.hasOwnProperty(key)) {
+            size++;
+        }
+      }
+      return size;
   };
 
   return someInstance;
