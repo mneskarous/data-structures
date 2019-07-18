762d17119479e7a877e1bdf495658b69bb124e0e
Michael Eskarous <mneskarous@gmail.com>
2019-07-17 20:35:07 -0700

    Implemented function for ES6 instantiation for the stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,28 @@
 class Stack {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
   constructor() {
+    this.storage = {};
+    this.length = 0;
+  }
+
+  push(value) {
+  this.storage[this.length] = value;
+  this.length++;
+  }
+
+pop() {
+  var popped = this.storage[this.length - 1];
+  delete this.storage[this.length - 1];
+  this.length--;
+  return popped;
+  }
+
+size() {
+  if (this.length < 0) {
+    return 0;
+  }
+  return this.length;
   }
 
 }
+
