3a2c9211b79b3e08e118a21a7869f9b0dd578475
Michael Eskarous <mneskarous@gmail.com>
2019-07-17 18:22:46 -0700

    Implemented function for functional-shared instantiation of the queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,43 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+
+  someInstance.count1 = 0;
+  someInstance.count2 = 0;
+  someInstance.length = 0;
+
+  extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var extend = function(obj, methods) {
+  for (var key in methods) {
+    obj[key] = methods[key];
+  }
+}
+
+var queueMethods = {
+  enqueue: function(value) {
+    this.count1++;
+    this.storage[this.count1] = value;
+    this.length++;
+  },
+
+  dequeue: function() {
+    this.count2++;
+    var dequeued = this.storage[this.count2];
+    delete this.storage[this.count2];
+    this.length--;
+    return dequeued;
+  },
+
+size: function() {
+  if (this.length < 0) {
+    return 0;
+  }
+  return this.length;
+  }
+};
 
 
