cea898407449a8b7a2de25098d23b6a63fdcb960
Michael Eskarous <mneskarous@gmail.com>
2019-07-17 19:37:02 -0700

    Implemented function for prototypal instantiaion of the stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+
+  someInstance.storage = {};
+  someInstance.length = 0;
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.length] = value;
+    this.length++;
+  },
+
+  pop: function() {
+    var popped = this.storage[this.length - 1];
+    delete this.storage[this.length - 1];
+    this.length--;
+    return popped;
+  },
+
+  size: function() {
+    if (this.length < 0) {
+      return 0;
+    }
+    return this.length;
+  }
+};
 
 
