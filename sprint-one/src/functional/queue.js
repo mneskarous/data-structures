6eba54f40fbef019f637c0a8cddd9587ba01fa35
Michael Eskarous <mneskarous@gmail.com>
2019-07-18 11:04:57 -0700

    Refactored function to find size of object after enqueuing and dequeuing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,29 +5,29 @@ var Queue = function() {
   var storage = {};
   var count1 = 0;
   var count2 = 0;
+  var length = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
     count1++;
     storage[count1] = value;
+    length++;
   };
 
   someInstance.dequeue = function() {
     count2++;
     var dequeued = storage[count2];
     delete storage[count2];
+    length--;
     return dequeued;
   }
 
   someInstance.size = function() {
-    var size = 0;
-      for (var key in storage) {
-        if (storage.hasOwnProperty(key)) {
-            size++;
-        }
-      }
-      return size;
+    if (length < 0) {
+      return 0;
+    }
+    return length;
   };
 
   return someInstance;
