042497a02cb5bdc57bd2200c6015504dd0c2c75f
Michael Eskarous <mneskarous@gmail.com>
2019-07-17 20:40:49 -0700

    Implemented function for ES6 instantiation for the queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,33 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
   constructor() {
+
+    this.storage = {};
+    this.count1 = 0;
+    this.count2 = 0;
+    this.length = 0;
+
+  }
+
+  enqueue(value) {
+    this.count1++;
+    this.storage[this.count1] = value;
+    this.length++;
+  }
+
+  dequeue() {
+    this.count2++;
+    var dequeued = this.storage[this.count2];
+    delete this.storage[this.count2];
+    this.length--;
+    return dequeued;
+  }
+
+  size() {
+    if (this.length < 0) {
+      return 0;
+    }
+    return this.length;
   }
 
 }
