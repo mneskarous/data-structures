c04bde3c3820ad5451b632ed36fa0b136b29b19c
Michael Eskarous <mneskarous@gmail.com>
2019-07-18 20:51:34 -0700

    Passed tests for list.addToTail and list.removeHead

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,15 +3,49 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = list.head;
+    } else {
+      list.tail.next = newNode;
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    var removed = list.head.value;
+    list.head = list.head.next;
+    return removed;
   };
 
   list.contains = function(target) {
-  };
+    // for (var node in list) {
+    //   if (target === node.value) {
+    //     return true;
+    //   }
+    //   return false;
+    // }
 
+    // set current node to first node;
+    var currentNode = list.head;
+    // while there is a current node
+      // if target === currentNode.value
+        // return true
+      // else if target !== currentNode.value
+        // set current node to the next current node
+      // in the event that
+    while (currentNode) {
+      if (target === currentNode.value) {
+        return true;
+      } else if (target !== currentNode.value) {
+        currentNode = currentNode.next;
+      }
+      return false;
+    }
+  };
   return list;
 };
 
@@ -26,4 +60,5 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+    Linear
  */
