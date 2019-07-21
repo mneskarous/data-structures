10e5851b51aebfddb82415299fad10212b7d48bf
Michael Eskarous <mneskarous@gmail.com>
2019-07-20 18:00:43 -0700

    Added pseudocode

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,22 @@
 var BinarySearchTree = function(value) {
+  // max 2 child nodes per node
+  // value 8
+  // left 6
+  // right 10
+  this.value = value;
 };
+
+
+// insert
+  // check to see if input value is smaller or larger than next value
+    // if smaller, go left
+    // else go right
+
+// contains
+  // same as insert except checking to see if value in tree
+
+// depth first log
+  //
 
 
 /*
