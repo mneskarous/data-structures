8a9414c9bbae84ecbb1c8341916569ffffd8921d
Michael Eskarous <mneskarous@gmail.com>
2019-07-21 13:53:31 -0700

    Implemented insert and contains methods

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,22 +1,84 @@
 var BinarySearchTree = function(value) {
+  var newBinarySearchTree = Object.create(binarySearchTreeMethods);
   // max 2 child nodes per node
-  // value 8
-  // left 6
-  // right 10
-  this.value = value;
+
+  newBinarySearchTree.value = value;
+  newBinarySearchTree.left = null;
+  newBinarySearchTree.right = null;
+
+  return newBinarySearchTree;
 };
 
+var binarySearchTreeMethods = {
+  insert: function(num) {
+    var node = BinarySearchTree(num);
+    var helper = function(obj) {
+      if (num < obj.value) {
+        if (obj.left === null) {
+          obj.left = node;
+      } else {
+        helper(obj.left);
+      }
+      } else {
+        if (obj.right === null) {
+          obj.right = node;
+      } else {
+        helper(obj.right)
+      }
+      }
+    }
+    helper(this);
+  },
 
-// insert
-  // check to see if input value is smaller or larger than next value
-    // if smaller, go left
-    // else go right
+  contains: function(target) {
+    // same as insert except checking to see if value in tree
+    if (target === this.value) {
+      return true;
+    } else if (target < this.value) {
+      if (this.left !== null) {
+        if (this.left.contains(target)) {
+          return true;
+        }
+      }
+      return false;
+    } else if (target > this.value) {
+      if (this.right !== null) {
+        if (this.right.contains(target)) {
+          return true;
+        }
+      }
+      return false;
+    } else {
+      return false;
+    }
+    // var helper = function(obj) {
+    //   if (target === obj.value) {
+    //     return true;
+    //   } else if (target < obj.value) {
+    //     if (helper(obj.left)) {
+    //       return true;
+    //     }
+    //   } else {
+    //     if (helper(obj.right)) {
+    //       return true;
+    //     }
+    //   }
+    //   return false;
+    // }
 
-// contains
-  // same as insert except checking to see if value in tree
 
-// depth first log
-  //
+    // if target is equal to value
+      // return true
+    // else if target is less than value
+      //
+  },
+
+  depthFirstLog: function(callback) {
+
+  }
+
+}
+
 
 
 /*
