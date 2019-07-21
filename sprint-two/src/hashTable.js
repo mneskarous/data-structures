2eb317dcd80f23f5f549ae8704def694a1b636cc
Michael Eskarous <mneskarous@gmail.com>
2019-07-21 11:18:06 -0700

    Fixed bug in insert method to handle collsions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -11,23 +11,18 @@ HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   if (this._storage[index] === undefined) {
     this._storage[index] = [[k, v]];
-  } else {
-    if (this._storage[index].length) {
-      for (var i = 0; i < this._storage[index].length; i++) {
-        if (this._storage[index][i][0] === k) {
+  }
+  if (this._storage[index].length > 0) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][0] === k) {
         this._storage[index][i][1] = v;
+      } else {
+        this._storage[index].push([k, v]);
       }
     }
   } else {
     this._storage[index].push([k, v]);
-    }
   }
-  // if there is no bucket (undefined)
-    // create a new bucket and set it to a tuple
-  // else
-    // push a new tuple into the bucket
-  //
-
 };
 
 HashTable.prototype.retrieve = function(k) {
