3bcf7eb14782510ae80c9376c3221efc79e06316
Michael Eskarous <mneskarous@gmail.com>
2019-07-28 18:45:25 -0700

    Refactored

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,34 +5,50 @@ var LinkedList = function() {
 
 
   list.addToTail = function(value) {
-    var newNode = new Node(value);
-    if (this.head === null) {
+    // create new node and set it to a new node object with the input value
+    var newNode = Node(value);
+    // if there is no head
+    if (!list.head) {
+      // set the head to the new node
       this.head = newNode;
-      this.tail = this.head;
-    } else {
-      this.tail.next = newNode;
-      this.tail = this.tail.next;
     }
+    // if there is a tail
+      // set the new tail to the new node
+    if (list.tail) {
+      this.tail.next = newNode;
+    }
+    // otherwise, set the tail to the new node
+    this.tail = newNode;
   };
 
   list.removeHead = function() {
-    var removed = this.head.value;
+    // create the current head and set it to the head
+    var currentHead = this.head;
+    // set the head to the node after the head
     this.head = this.head.next;
-    return removed;
+    // return the value of the current head
+    return currentHead.value;
   };
 
-list.contains = function(target) {
-  var currentNode = this.head;
-  while (currentNode) {
-    if (currentNode.value === target) {
+  list.contains = function(target) {
+    // set the current node to the head
+    var currentNode = this.head;
+    // while there is a current node
+    while (currentNode) {
+      // if the value of the current node is the target
+      if (currentNode.value === target) {
+        // return true
         return true;
-    }
+      }
+      // set the current node to the node after the current node
       currentNode = currentNode.next;
     }
+    // return false;
     return false;
+    };
+
+    return list;
   };
-  return list;
-};
 
 var Node = function(value) {
   var node = {};
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -12,22 +12,48 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  this.children.push(Tree(value));
+  // create child and set it to a new tree object with the input as the value
+  var child = Tree(value);
+  // push the child into the children array
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
-  if (target === this.value) {
+  // base case
+  // if the value is the target
+  if (this.value === target) {
+    // return true
     return true;
   }
+  // recursive case
+  // iterate through the children array
   for (var i = 0; i < this.children.length; i++) {
+    // if the current item in the children array contains the target
     if (this.children[i].contains(target)) {
+      // return true
       return true;
     }
   }
+  // return false
   return false;
 };
 
-
+treeMethods.traverse = function(callback) {
+  // invoke the callback with the value as the parameter
+  callback(this.value);
+  // base case
+  // if there is no children array
+  if (!this.children) {
+    // stop
+    return;
+  }
+  // recursive case
+  // otherwise, iterate through the children array
+  for (var i = 0; i < this.children.length; i++) {
+    // invoke the traverse method recursively on the current child
+    this.children[i].traverse(callback);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
